5e22f87b52fd6f3c7aee16b30a1ea4b5e6a0d031
qiushaocloud <qiushaocloud@gmail.com>
2023-06-07 11:31:35 +0800

    update sh

--- docker-entrypoint-ex.sh
+++ docker-entrypoint-ex.sh
@@ -3,19 +3,14 @@ set -Eeuo pipefail
 
 SSL_KEY_PATH_TMP='/ssl_cetrts/server.key'
 SSL_CRT_PATH_TMP='/ssl_cetrts/server.pem'
-FORCE_HTTPS_TMP='yes'
 if [ $SSL_KEY_PATH ]; then
     SSL_KEY_PATH_TMP=$SSL_KEY_PATH
 fi
 if [ $SSL_CRT_PATH ]; then
     SSL_CRT_PATH_TMP=$SSL_CRT_PATH
 fi
-if [ $FORCE_HTTPS ]; then
-    FORCE_HTTPS_TMP=$FORCE_HTTPS
-fi
 echo "SSL_KEY_PATH_TMP: $SSL_KEY_PATH_TMP"
 echo "SSL_CRT_PATH_TMP: $SSL_CRT_PATH_TMP"
-echo "FORCE_HTTPS_TMP: $FORCE_HTTPS_TMP"
 
 if [ -f "$SSL_KEY_PATH_TMP" ] && [ -f "$SSL_CRT_PATH_TMP" ]; then
     echo "exsit ssl need files, use https"
@@ -32,13 +27,26 @@ if [ -f "$SSL_KEY_PATH_TMP" ] && [ -f "$SSL_CRT_PATH_TMP" ]; then
             echo "change default-ssl.conf /ssl_cetrts/server.key to $SSL_KEY_PATH_TMP"
             sed -i s#/ssl_cetrts/server.key#$SSL_KEY_PATH_TMP#g /etc/apache2/sites-enabled/default-ssl.conf
         fi
+
+        if [ $WP_CONTAINER_PORT ] && [ "$WP_CONTAINER_PORT" != "443" ]; then
+            echo "https use other port, WP_CONTAINER_PORT: $WP_CONTAINER_PORT"
+            sed -i s#:443/:$WP_CONTAINER_PORT/g /etc/apache2/sites-enabled/default-ssl.conf
+        fi
     fi
     
-    if [ "$FORCE_HTTPS_TMP" == "yes" ]; then
-        if [ "`md5sum /etc/apache2/sites-enabled/000-default.conf`" == "`cat /etc/apache2/sites-ssl-conf-available/source-000-default-md5sum`" ]; then
-            echo "/etc/apache2/sites-enabled/000-default.conf is default file"
-            echo "cp /etc/apache2/sites-enabled/000-default.conf"
-            cp -ra /etc/apache2/sites-ssl-conf-available/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+    if [ "`md5sum /etc/apache2/sites-enabled/000-default.conf`" == "`cat /etc/apache2/sites-ssl-conf-available/source-000-default-md5sum`" ]; then
+        echo "/etc/apache2/sites-enabled/000-default.conf is default file, but is https env, need move file"
+        echo "mv /etc/apache2/sites-enabled/000-default.conf"
+        mkdir -p /etc/apache2/sites-enabled-bak
+        mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled-bak/000-default.conf
+    fi
+else
+    echo "not exsit ssl need files, use http"
+    if [ "`md5sum /etc/apache2/sites-enabled/000-default.conf`" == "`cat /etc/apache2/sites-ssl-conf-available/source-000-default-md5sum`" ]; then
+        echo "/etc/apache2/sites-enabled/000-default.conf is default file, current is http env"
+        if [ $WP_CONTAINER_PORT ] && [ "$WP_CONTAINER_PORT" != "443" ]; then
+            echo "http use other port, WP_CONTAINER_PORT: $WP_CONTAINER_PORT"
+            sed -i s#:80/:$WP_CONTAINER_PORT/g /etc/apache2/sites-enabled/000-default.conf
         fi
     fi
 fi
